6f3cb1e60b65af0f0128f218cf8e98c98e08b6db
Madelenkr <142605372+Madelenkr@users.noreply.github.com>
2025-04-03 11:27:02 +0200

    navigering
    Laget navigering til medlemmene

--- Gruppe38/src/App.jsx
+++ Gruppe38/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
+import { Component, useState } from 'react'
 import Home from './components/Home'
 import "./style/style.css";
+import { Route, Router, Routes } from 'react-router-dom';
+import MemberPage from "./components/MemberPage";
+
 
 
 function App() {
-  return <Home />
-  const [count, setCount] = useState(0)
-
-  return (
+  return(
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+     <Routes>
+      <Route path="/"element={<Home />} />
+      <Route path="medlem/:id" element={<MemberPage />} />
+     </Routes>
+   </>
   )
 }
 
